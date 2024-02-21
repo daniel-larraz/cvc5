737dc6d49a4b271f3ca93385a0336979a3317a48
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-02-21 12:47:35 -0600

    cbw-all test 18

--- contrib/cibw_before_all_windows.sh
+++ contrib/cibw_before_all_windows.sh
@@ -7,12 +7,6 @@ echo "include_dirs=$(cygpath -m $(pwd)/install/include)" >> setup.cfg
 echo "library_dirs=$(cygpath -m $(pwd)/install/lib)" >> setup.cfg
 cat setup.cfg
 
-pacman -S --noconfirm \
-    make\
-    mingw-w64-x86_64-cmake\
-    mingw-w64-x86_64-gcc\
-    mingw-w64-x86_64-gmp
-
 python3 -m pip install --user -r contrib/requirements_build.txt
 
 ./configure.sh production --auto-download --win64-native --prefix=./install

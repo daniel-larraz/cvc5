7959b912f56e15ca1f24ffd6b9db904db455c9e3
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-02-21 10:11:42 -0600

    cbw-all test 14

--- contrib/cibw_before_all_windows.sh
+++ contrib/cibw_before_all_windows.sh
@@ -14,7 +14,7 @@ pacman -S --noconfirm \
 
 python3 -m pip install --user -r contrib/requirements_build.txt
 
-./configure.sh production --auto-download --prefix=./install
+./configure.sh production --auto-download --win64-native --prefix=./install
 
 pushd build
 make -j$(( $(nproc) + 1 ))

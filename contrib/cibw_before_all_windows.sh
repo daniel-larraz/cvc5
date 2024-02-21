cc345c1f05087253f7821d206d71c54e0bc98c4c
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-02-21 14:21:17 -0600

    cbw-all test 20

--- contrib/cibw_before_all_windows.sh
+++ contrib/cibw_before_all_windows.sh
@@ -4,7 +4,7 @@ echo "[build]" > setup.cfg
 echo "compiler = mingw32" >> setup.cfg
 echo "[build_ext]" >> setup.cfg
 echo "include_dirs=$(cygpath -m $(pwd)/install/include)" >> setup.cfg
-echo "library_dirs=$(cygpath -m $(pwd)/install/lib);C:\Windows\SysWOW64" >> setup.cfg
+echo "library_dirs=$(cygpath -m $(pwd)/install/lib)" >> setup.cfg
 cat setup.cfg
 
 python3 -m pip install --user -r contrib/requirements_build.txt

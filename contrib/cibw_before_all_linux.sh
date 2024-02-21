e3b9aa05061a2c1fb74a1a14183d85909afbf451
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-02-20 23:32:00 -0600

    cbw-linux test 4

--- contrib/cibw_before_all_linux.sh
+++ contrib/cibw_before_all_linux.sh
@@ -7,6 +7,3 @@ python3 -m pip install --user -r contrib/requirements_build.txt
 pushd build
 make -j$(( $(nproc) + 1 ))
 popd
-
-export CPLUS_INCLUDE_PATH=./include:./build/include
-export LIBRARY_PATH=./build/src:./build/src/parser

1ef3e5d9b089a6808351580b934425faf5cf0786
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-02-21 07:53:11 -0600

    cbw-linux test 6

--- contrib/cibw_before_all_linux.sh
+++ contrib/cibw_before_all_linux.sh
@@ -2,7 +2,7 @@
 
 python3 -m pip install --user -r contrib/requirements_build.txt
 
-./configure.sh production --auto-download --prefix install
+./configure.sh production --auto-download --prefix=./install
 
 pushd build
 make -j$(( $(nproc) + 1 ))

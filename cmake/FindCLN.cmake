233842c14a58d4986e48bcdf81e43e6d9d64d335
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-05-06 19:28:09 -0500

    Test CLN

--- cmake/FindCLN.cmake
+++ cmake/FindCLN.cmake
@@ -91,6 +91,7 @@ if(NOT CLN_FOUND_SYSTEM)
     set(CLN_BYPRODUCTS <INSTALL_DIR>/${CMAKE_INSTALL_LIBDIR}/libcln.${CMAKE_STATIC_LIBRARY_SUFFIX})
   endif()
 
+  set(CONFIGURE_OPTS "")
   # CLN yields the following message at the end of the build process.
   #     WARNING: `makeinfo' is missing on your system.
   # This is a specific issue to Github CI on linux environments.
@@ -98,24 +99,30 @@ if(NOT CLN_FOUND_SYSTEM)
   # it is possible to get around this issue by just disabling it:
   set(CONFIGURE_ENV env "MAKEINFO=true")
 
-  if(${CMAKE_VERSION} VERSION_LESS "3.29")
-    set(CLN_URL "https://www.ginac.de/CLN/cln.git/?p=cln.git\\\;a=snapshot\\\;h=cln_${CLN_TAG}\\\;sf=tgz")
-  else()
-    set(CLN_URL "https://www.ginac.de/CLN/cln.git/?p=cln.git\;a=snapshot\;h=cln_${CLN_TAG}\;sf=tgz")
+  if(CMAKE_CROSSCOMPILING OR CMAKE_CROSSCOMPILING_MACOS)
+    set(CONFIGURE_OPTS
+      --host=${TOOLCHAIN_PREFIX}
+      --build=${CMAKE_HOST_SYSTEM_PROCESSOR})
+
+    set(CONFIGURE_ENV ${CONFIGURE_ENV} ${CMAKE_COMMAND} -E
+      env "CC_FOR_BUILD=cc")
+    if (CMAKE_CROSSCOMPILING_MACOS)
+      set(CONFIGURE_ENV
+        ${CONFIGURE_ENV}
+        env "CFLAGS=--target=${TOOLCHAIN_PREFIX}"
+        env "LDFLAGS=-arch ${CMAKE_OSX_ARCHITECTURES}")
+    endif()
   endif()
 
   ExternalProject_Add(
     CLN-EP
     ${COMMON_EP_CONFIG}
-    URL ${CLN_URL}
-    URL_HASH SHA1=bd6dec17cf1088bdd592794d9239d47c752cf3da
+    URL "https://www.ginac.de/CLN/cln-1.3.7.tar.bz2"
+    URL_HASH SHA1=17cf2c60e262e30f57caae39692fce7917e11d95
     DOWNLOAD_NAME cln.tgz
     CONFIGURE_COMMAND
-      ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> ./autogen.sh
-    COMMAND
-      ${CMAKE_COMMAND} -E chdir <SOURCE_DIR> autoreconf -iv
-    COMMAND ${CONFIGURE_ENV} ${SHELL} <SOURCE_DIR>/configure --prefix=<INSTALL_DIR>
-            ${LINK_OPTS} --with-pic
+      ${CONFIGURE_ENV} ${SHELL} <SOURCE_DIR>/configure
+        --prefix=<INSTALL_DIR> ${LINK_OPTS} --with-pic
     BUILD_BYPRODUCTS ${CLN_BYPRODUCTS}
   )
 

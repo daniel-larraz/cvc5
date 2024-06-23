9fd0fff9036d79b4ce58835ea5dad060d478e728
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2024-06-23 16:31:27 +0200

    Only replace on Windows

--- cmake/install_python_wheel.cmake
+++ cmake/install_python_wheel.cmake
@@ -18,7 +18,9 @@ execute_process(COMMAND
 file(GLOB WHL_FILE ${REPAIRED_WHEEL_DIR}/cvc5*.whl)
 
 string(REPLACE "\"" "" INSTALL_CMD "${INSTALL_CMD}")
-string(REPLACE "/" "\\" INSTALL_CMD "${INSTALL_CMD}")
+if(WIN32)
+  string(REPLACE "/" "\\" INSTALL_CMD "${INSTALL_CMD}")
+endif()
 set(INSTALL_CMD "${INSTALL_CMD} ${WHL_FILE}")
 separate_arguments(INSTALL_CMD)
 message(STATUS "INSTALL_CMD: " ${INSTALL_CMD})

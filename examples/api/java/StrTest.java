34832e1eb719874b939c0b591aea1f98af31ddf4
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2025-02-27 15:48:11 -0600

    Test

---
+++ examples/api/java/StrTest.java
@@ -0,0 +1,15 @@
+import io.github.cvc5.*;
+
+public class StrTest {
+  public static void main(String args[]) throws CVC5ApiException {
+    TermManager tm = new TermManager();
+
+    Term str = tm.mkString("\\u{200cb}", true);
+  
+    String value = str.getStringValue();
+    String expected = Character.toString(0x200cb);
+
+    assert value.equals(expected);
+    System.out.println("Value: " + value);
+  }
+}

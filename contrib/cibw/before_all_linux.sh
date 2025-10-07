634d5c04daf73372b18892d407b7c98ba17fe70b
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2025-10-07 07:02:29 -0500

    --no-pyvenv

--- contrib/cibw/before_all_linux.sh
+++ contrib/cibw/before_all_linux.sh
@@ -2,7 +2,7 @@
 
 GPL="$1"
 
-COMMON_CMD="./configure.sh production --auto-download --python-bindings --python-only-src --prefix=./install"
+COMMON_CMD="./configure.sh production --auto-download --python-bindings --python-only-src --prefix=./install --no-pyvenv"
 
 if [ "$GPL" = "true" ]; then
   GPL_FLAGS="--gpl --cln --glpk --cocoa"

71a18709b9911525b002898db38441cc5a87ff50
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2026-02-14 19:18:10 -0600

    Enable dangling-reference warning

--- src/expr/node.h
+++ src/expr/node.h
@@ -564,7 +564,7 @@ public:
    * If this is a CONST_* Node, extract the constant from it.
    */
   template <class T>
-  inline const T& getConst() const;
+  [[gnu::no_dangling]] inline const T& getConst() const;
 
   /**
    * @return true if this is a skolem function.

442f1d8b75cb913311cecd0eafd851ccd7f3edac
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2026-04-23 16:40:05 -0500

    Remove copy methods

--- src/util/floatingpoint_size.h
+++ src/util/floatingpoint_size.h
@@ -30,10 +30,6 @@ class FloatingPointSize
  public:
   /** Constructors. */
   FloatingPointSize(uint32_t exp_size, uint32_t sig_size);
-  FloatingPointSize(const FloatingPointSize& other);
-
-  /** Copy assignment. */
-  FloatingPointSize& operator=(const FloatingPointSize& other);
 
   /** Operator overload for equality comparison. */
   bool operator==(const FloatingPointSize& fps) const
